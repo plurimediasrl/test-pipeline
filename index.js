fcb4cbba1de45d8cb7ecc308486deaee52c16641
teoc87 <matteo.cozzoli@plurimedia.it>
2019-10-08 17:24:22 +0200

    iuhgf

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res, next) => {
-    res.send('Hello plurimedia!')
+    res.send('Hello Pluriplurimedia! '+ parseInt(Math.random()*100))
 })
 app.listen(process.env.PORT || 3000)
