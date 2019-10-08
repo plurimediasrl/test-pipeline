1bc13d4cd0429b1d0c6971864fdd0820332e9d54
teoc87 <matteo.cozzoli@plurimedia.it>
2019-10-08 17:29:08 +0200

    tre

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res, next) => {
-    res.send('Hello Pluriplurimedia! '+ parseInt(Math.random()*100))
+    res.send('Hello Pluriplurimedia! '+ parseInt(Math.random()*10000))
 })
 app.listen(process.env.PORT || 3000)
